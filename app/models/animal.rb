41e87fae34e8bb988a23099809d6266137df8809
Ricardo Mattos <ricardo.svmtts@gmail.com>
2018-04-15 16:57:51 +0000

    animal validates fix

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,6 +1,6 @@
 class Animal < ActiveRecord::Base
     mount_uploader :avatar, AvatarUploader
-    validates :name, :age, :specie, :breed, :avatar, presence: true
+    validates :name, :age, :specie, :breed, presence: true
 
     def getAttendance
         Attendance.where(:animal_id => self.id)
